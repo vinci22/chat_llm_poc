b5698614c8e6da8955aeab4fac3e57ec15c02a79
unknown <vinci22>
2024-09-16 17:42:58 -0500

    added configs for railway

--- llm_utils/main.py
+++ llm_utils/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from graph import FriendlyResponder
 import uvicorn
 
 app = FastAPI()
 responder = FriendlyResponder()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar dominios específicos en lugar de "*"
+    allow_credentials=True,
+    allow_methods=["*"],  # Puedes restringir a métodos específicos como ["GET", "POST"]
+    allow_headers=["*"],  # Puedes especificar los headers permitidos como ["Authorization", "Content-Type"]
+)
 
 class MessageRequest(BaseModel):
     user_input: str
@@ -15,12 +25,11 @@ async def respond_to_message(request: MessageRequest):
     try:
         user_input = request.user_input
         session_id = request.session_id
-        message = {"question":user_input,"session_id":session_id}
+        message = {"question": user_input, "session_id": session_id}
         responses = responder.run(message)
         return {"responses": responses}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8001)
